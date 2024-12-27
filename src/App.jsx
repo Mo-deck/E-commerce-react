0f2b8e2256900d14f9e19f420c7dcbdd0cdd1903
Mo-Deck <mohamedabow6774@gmail.com>
2024-12-27 23:12:04 +0300

    Adding to file such as Header,Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 function App() {
 
-  return <h1>Mohamed</h1>
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
 }
 
 export default App
