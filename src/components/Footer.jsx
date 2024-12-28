aebfac52369ae7ea9d32824aed5e3949b6aeef3d
Mo-Deck <mohamedabow6774@gmail.com>
2024-12-28 23:31:50 +0300

    design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="mt-20 bg-gray-800 text-white py-4">
+      <div className="max-x-4xl mx-auto text-center">
+        <p>&copy; 2025  All rights reserved</p>
+      </div>
+    </div>
   )
 }
 
