2cd9f8438711a39df46b7a074207628db6d37244
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-01 17:39:34 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="mt-20 bg-gray-800 text-white py-4">
-      <div className="max-x-4xl text-center">
-        <p>&copy;All rights reserved</p>
+    <div className='bg-sky-500 border-b-2 border-white px-18 py-12'>
+      <div className='border-b-2 py-10 mx-10 '>
+
+     
+      <div className='flex justify-between  items-center'>
+        <div className='w-[300px]'>
+          <h1 className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+        </div>
+        <div className='flex gap-2 items-center'>
+          <input className='px-8 rounded-md py-2 bg-white' type="text" placeholder='Enter your email' />
+          <button className='py-2 rounded-md px-3 bg-white text-black'>Subscribe</button>
+        </div>
       </div>
+      </div>
+
+      <div>
+        
+      </div>
+
+
     </div>
   )
 }
